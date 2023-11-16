025cff204057f92f707ac44be6380eb7676ab996
fierychicken65 <fadiltutu@gmail.com>
2023-11-16 22:40:00 +0530

    Refactor Navbar component to handle user authentication and add click handlers.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,53 @@
 import React from "react";
 import { Link,useNavigate } from "react-router-dom";
 
-export default function Navbar(){
+
+
+export default function Navbar() {
    const auth = localStorage.getItem("user");
    const navigate = useNavigate();
-   const logout = () =>{
+   const logout = () => {
       localStorage.clear();
-      navigate("/login");  
-   }
-   return(
+      navigate("/login");
+   };
+
+   const handleClick = () => {
+      if (!auth) {
+         alert("Please log in to access this page.");
+      }
+   };
+
+   return (
       <nav>
-         <img src="./images/logo.png" alt="nope" height="50px"/>
-         <h1 className="nav-head">uction Site</h1>
+         <img src="./images/logo.png" alt="nope" height="50px" onClick={handleClick} />
+         <h1 className="nav-head" onClick={handleClick}>uction Site</h1>
          <ul className="nav-ele">
-            <li><Link to="/Home">Home</Link></li>
-            <li><Link to="/About">About</Link></li>
-            <li><Link to="/Contact">Contact Us</Link></li>
-            <li>{auth ? <Link to="/logout" onClick={logout}>Logout</Link> : <Link to="/login">Sign up</Link>}</li>
+            <li>
+               <Link to="/Home" onClick={handleClick}>
+                  Home
+               </Link>
+            </li>
+            <li>
+               <Link to="/About" onClick={handleClick}>
+                  About
+               </Link>
+            </li>
+            <li>
+               <Link to="/Contact" onClick={handleClick}>
+                  Contact Us
+               </Link>
+            </li>
+            <li>
+               {auth ? (
+                  <Link to="/logout" onClick={logout}>
+                     Logout
+                  </Link>
+               ) : (
+                  <Link to="/login">Sign up</Link>
+               )}
+            </li>
          </ul>
       </nav>
-   )
+   );
 }
 
